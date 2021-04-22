43537e30a28ee021837fbcfb0d6a5e9aa23fff35
Ahmed Zobayer <a.zobayer@3dvi.com>
2021-04-23 01:11:21 +0600

    portfolio updated

--- projects/redishketch-romeo/testServer/post.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, GET, POST");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-
-$data = $_POST;
-echo json_encode($data, true);
